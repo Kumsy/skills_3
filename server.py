e2b7ed33c7aa36b0ce994a7891ed84d2d7d6dc2d
vagrant <vagrant@vagrant.vm>
2019-04-29 07:15:19 +0000

    Main Lab complete

--- server.py
+++ server.py
@@ -35,17 +35,49 @@ MOST_LOVED_MELONS = {
 }
 
 # YOUR ROUTES GO HERE
+# Homepage
 @app.route('/')
-def index():
+def homepage():
 
-    return """
-      <html>
-        <body>
-          <h1>Hi</h1>
-        </body>
-      </html>
-      """
+    if 'user_name' in session:
+        return redirect('/top-melons')
+    return render_template('homepage.html')
 
+# Store username in a session
+@app.route('/get-name')
+def get_name():
+
+    user_name = request.args.get("user_name")
+    session['user_name'] = user_name
+    # print(session['user_name'])
+
+    return redirect('/top-melons')
+
+
+# Shows our most loved melons page
+@app.route('/top-melons')
+def top_melons():
+
+    name = []
+    num_loves = []
+    melon_image = []
+
+    for melons in MOST_LOVED_MELONS:
+        name.append(MOST_LOVED_MELONS[melons]['name'])
+        num_loves.append(MOST_LOVED_MELONS[melons]['num_loves'])
+        melon_image.append(MOST_LOVED_MELONS[melons]['img'])
+
+    most_mels = MOST_LOVED_MELONS
+
+    if 'user_name' in session:
+        return render_template("top-melons.html",
+                                name = name,
+                                num_loves = num_loves,
+                                melon_image = melon_image,
+                                MOST_LOVED_MELONS = most_mels,
+                                user_name = session['user_name']
+                                )
+    return redirect('/')
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
